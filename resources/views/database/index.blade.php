4fa03cc3a5790f9f90177a27c081bc09f9292af4
Jeff Puckett <jeff@jeffpuckett.com>
2017-05-11 22:06:20 +0000

    dry up views

--- resources/views/database/index.blade.php
+++ resources/views/database/index.blade.php
@@ -1,17 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Databases</div>
 
-                <div class="panel-body">
-                    Here is some information about your databases.
-                </div>
-            </div>
-        </div>
+<div class="panel panel-default">
+    <div class="panel-heading">Databases</div>
+
+    <div class="panel-body">
+        Here is some information about your databases.
     </div>
 </div>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,7 +81,16 @@
             </div>
         </nav>
 
-        @yield('content')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+
+                    @yield('content')
+
+                </div>
+            </div>
+        </div>
+
     </div>
 
     <!-- Scripts -->
