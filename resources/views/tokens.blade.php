9a54627f7dddc55db180d74c08d1e01cef57f01e
Jeff Puckett <jeff@jeffpuckett.com>
2017-05-13 03:17:42 +0000

    remove client oauth controls for now

--- resources/views/tokens.blade.php
+++ resources/views/tokens.blade.php
@@ -2,8 +2,6 @@
 
 @section('content')
 
-<passport-clients></passport-clients>
-<passport-authorized-clients></passport-authorized-clients>
 <passport-personal-access-tokens></passport-personal-access-tokens>
 
 @endsection
